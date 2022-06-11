ff520c370d3abafed06562fd45f41f893a815f83
Chyp Yura <85065859+chpuh@users.noreply.github.com>
2022-06-11 12:12:40 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+Home.onclick = () =>{
+    hom.style.display = 'flex';
+    aboutMe.style.display = 'none';
+    mProject.style.display = 'none';
+    son.style.display = 'none';
+}
+About.onclick = () =>{
+    hom.style.display = 'none';
+    aboutMe.style.display = 'flex';
+    mProject.style.display = 'none';
+    son.style.display = 'none';
+}
+MyProject.onclick = () =>{
+    hom.style.display = 'none';
+    aboutMe.style.display = 'none';
+    mProject.style.display = 'flex';
+    son.style.display = 'none';
+}
+Internet.onclick = () =>{
+    hom.style.display = 'none';
+    aboutMe.style.display = 'none';
+    mProject.style.display = 'none';
+    son.style.display = 'flex';
+}
